8c9a690374444f4d12347bebcac901d1f0cc0a20
Iskuhi Vardanyan <iskuhivardanyan1989@gmail.com>
2026-03-02 17:50:51 +0400

    First prompt lesson

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 "use strict";
  
-console.log(2);
+// console.log(2);
 
 // const result  = alert("Hello!");
-const result  = confirm("Hello!");
+// const result  = confirm("Hello!");
+
+    let numberOfFilms = prompt("how many movies have you watched", '');
+    let lastFilm1 = prompt("last watched film", '');
+    let filmPoint1 = prompt("which point", '');
+    let lastFilm2 = prompt("last watched film", '');
+    let filmPoint2 = prompt("which point", '');
+    let personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+    personalMovieDB.movies[lastFilm1] = filmPoint1;
+    personalMovieDB.movies[lastFilm2] = filmPoint2;
+
+    console.log(personalMovieDB.movies);
+    console.log(personalMovieDB);
